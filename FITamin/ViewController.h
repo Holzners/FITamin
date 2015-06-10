2469b9e6a531c9680c7db3f6b893f4d78327d8d6
holzners <von-mir1@gmx.de>
2015-06-10 16:29:50 +0200

    Initial Commit

---
+++ FITamin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FITamin
+//
+//  Created by Holzner on 10.06.15.
+//  Copyright (c) 2015 iOS-Praktikum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FITaminTests/FITaminTests.m
@@ -0,0 +1,40 @@
+//
+//  FITaminTests.m
+//  FITaminTests
+//
+//  Created by Holzner on 10.06.15.
+//  Copyright (c) 2015 iOS-Praktikum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface FITaminTests : XCTestCase
+
+@end
+
+@implementation FITaminTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
