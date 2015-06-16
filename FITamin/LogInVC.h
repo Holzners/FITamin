7927b0a237fdcd46927034e0302e5143d2e108bc
kyrillschmid <kyrillschmid@web.de>
2015-06-16 10:23:27 +0200

    LogInVC.m und LogInVC.h hinzugefügt

---
+++ FITamin/LogInVC.h
@@ -0,0 +1,13 @@
+//
+//  LogInVC.h
+//  FITamin
+//
+//  Created by admin on 16.06.15.
+//  Copyright (c) 2015 iOS-Praktikum. All rights reserved.
+//
+
+#ifndef FITamin_LogInVC_h
+#define FITamin_LogInVC_h
+
+
+#endif
---
+++ FITamin/LogInVC.m
@@ -0,0 +1,9 @@
+//
+//  LogInVC.m
+//  FITamin
+//
+//  Created by admin on 16.06.15.
+//  Copyright (c) 2015 iOS-Praktikum. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
