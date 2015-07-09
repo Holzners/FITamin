d9a5e7a1d42c90c908c8ad5a98dece789c451a5c
holzners <von-mir1@gmx.de>
2015-07-09 11:39:59 +0200

    ff

--- FITamin/LogoutVC.m
+++ FITamin/LogoutVC.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self performSegueWithIdentifier:@"backToLogIn" sender:self]
+    [self performSegueWithIdentifier:@"backToLogIn" sender:self];
     // Do any additional setup after loading the view.
 }
 
