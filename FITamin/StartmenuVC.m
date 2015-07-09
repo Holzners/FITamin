c80b44885e2065ac2979c2f864dd675b53801267
Julia Kinshofer <julia_kinshofer@me.com>
2015-07-09 11:21:08 +0200

    Tabbar swipe

--- FITamin/StartmenuVC.m
+++ FITamin/StartmenuVC.m
@@ -29,6 +29,30 @@
     self.tabBarController.tabBar.translucent = NO;
     [self.tabBarController setDelegate:self];
     
+    UISwipeGestureRecognizer *swipeLeft = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(tappedRightButton:)];
+    [swipeLeft setDirection:UISwipeGestureRecognizerDirectionLeft];
+    [self.view addGestureRecognizer:swipeLeft];
+    
+}
+
+- (IBAction)tappedRightButton:(id)sender
+{
+    NSUInteger selectedIndex = [self.tabBarController selectedIndex];
+    
+    UIView * fromView = self.tabBarController.selectedViewController.view;
+    UIView * toView = [[self.tabBarController.viewControllers objectAtIndex:selectedIndex+1] view];
+    
+    // Transition using a page curl.
+    [UIView transitionFromView:fromView
+                        toView:toView
+                      duration:0.5
+                       options:(selectedIndex+1 > self.tabBarController.selectedIndex ? UIViewAnimationOptionTransitionFlipFromLeft  :
+                                UIViewAnimationOptionTransitionFlipFromRight)
+                    completion:^(BOOL finished) {
+                        if (finished) {
+                            self.tabBarController.selectedIndex = selectedIndex+1;
+                        }
+                    }];
 }
 
 - (void)tabBarController:(UITabBarController *)tabBarController
